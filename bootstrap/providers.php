c51925f8ccfd5ad304aa263841950dffc416c802
Maksym Stepenko <maksmaksmaks@Maksyms-MacBook-Pro.local>
2025-11-17 22:25:37 -0500

    Disable NovaServiceProvider and rename nova.php config to fix 500 error

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,11 +4,11 @@ $providers = [
     App\Providers\AppServiceProvider::class,
 ];
 
-// Загружаем Nova только если пакет установлен
-// Проверяем наличие основного класса Nova перед загрузкой провайдера
-if (class_exists(\Laravel\Nova\Nova::class) && 
-    class_exists(\Laravel\Nova\NovaApplicationServiceProvider::class)) {
-    $providers[] = App\Providers\NovaServiceProvider::class;
-}
+// NovaServiceProvider временно отключен
+// Раскомментируйте после установки Nova
+// if (class_exists(\Laravel\Nova\Nova::class) && 
+//     class_exists(\Laravel\Nova\NovaApplicationServiceProvider::class)) {
+//     $providers[] = App\Providers\NovaServiceProvider::class;
+// }
 
 return $providers;
